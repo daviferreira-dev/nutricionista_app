c3f666c3d793424ee66ff42859523bbe2af23b56
davi <fdavi07@gmail.com>
2025-07-28 14:40:45 -0300

    início do mobile

---
+++ mobile-v1.0/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+export default App;
